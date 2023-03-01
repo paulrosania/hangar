0f564ad8e0d52b30128fa6f86e4c37e1085c8c47
Paul Rosania <paul.rosania@gmail.com>
2023-02-28 21:48:26 -0800

    initial oauth handshake implementation

--- go.mod
+++ go.mod
@@ -2,4 +2,14 @@ module github.com/paulrosania/hangar
 
 go 1.20
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/joho/godotenv v1.5.1
+	golang.org/x/oauth2 v0.5.0
+)
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	golang.org/x/net v0.6.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+)
--- main.go
+++ main.go
@@ -6,13 +6,33 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 
 	"github.com/joho/godotenv"
+	"golang.org/x/oauth2"
 )
 
 func cleanup() {
 	log.Println("🛬 Hangar server shutting down.")
+}
+
+func parseScopes(s string) []string {
+	return strings.Split(s, ",")
+}
+
+func loadConfig(provider string, baseUrl string) oauth2.Config {
+	scopes := parseScopes(os.Getenv(fmt.Sprintf("%s_DEFAULT_SCOPES", strings.ToUpper(provider))))
+	return oauth2.Config{
+		ClientID:     os.Getenv(fmt.Sprintf("%s_CLIENT_ID", strings.ToUpper(provider))),
+		ClientSecret: os.Getenv(fmt.Sprintf("%s_CLIENT_SECRET", strings.ToUpper(provider))),
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  os.Getenv(fmt.Sprintf("%s_AUTH_URL", strings.ToUpper(provider))),
+			TokenURL: os.Getenv(fmt.Sprintf("%s_TOKEN_URL", strings.ToUpper(provider))),
+		},
+		RedirectURL: fmt.Sprintf("%s/auth/%s/callback", baseUrl, provider),
+		Scopes:      scopes,
+	}
 }
 
 func main() {
@@ -25,6 +45,34 @@ func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
+	}
+
+	baseUrl := os.Getenv("BASE_URL")
+	if baseUrl == "" {
+		baseUrl = fmt.Sprintf("http://%s:%s", host, port)
+	}
+
+	configs := make(map[string]oauth2.Config)
+	for _, e := range os.Environ() {
+		parts := strings.Split(e, "=")
+		key := parts[0]
+		if strings.HasSuffix(key, "_CLIENT_ID") {
+			provider := strings.ToLower(strings.TrimSuffix(key, "_CLIENT_ID"))
+			conf := loadConfig(provider, baseUrl)
+			log.Printf("Detected '%s' configuration.\n", provider)
+			configs[provider] = conf
+			http.HandleFunc(fmt.Sprintf("/auth/%s", provider), func(w http.ResponseWriter, r *http.Request) {
+				authURL := conf.AuthCodeURL("state")
+				http.Redirect(w, r, authURL, http.StatusFound)
+			})
+			http.HandleFunc(fmt.Sprintf("/auth/%s/callback", provider), func(w http.ResponseWriter, r *http.Request) {
+				token, err := conf.Exchange(oauth2.NoContext, r.URL.Query().Get("code"))
+				if err != nil {
+					fmt.Fprintf(w, "Error: %s", err)
+				}
+				fmt.Fprintf(w, "Token: %+v", token)
+			})
+		}
 	}
 
 	// Register for SIGTERM (Ctrl-C)
